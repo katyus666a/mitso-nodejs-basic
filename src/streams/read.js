cc45e1c40496f49fbdc7cd4cedd92b085a172938
katyus666a <kruekaterinaresakevich@gmail.com>
2025-05-02 11:50:47 +0300

    feat: completed wtite

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+
+import { createReadStream } from 'fs';
+import path from 'path';
+
 const read = async () => {
     // Write your code here 
+    const filePath = path.resolve('files/fileToRead.txt');
+    const stream = createReadStream(filePath, 'utf-8');
+    stream.pipe(process.stdout);
 };
 
+await read();
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+
+import { Transform } from 'stream';
+
 const transform = async () => {
     // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().trim().split('').reverse().join('') + '\n';
+            callback(null, reversed);
+        }
+    });
+
+    process.stdin.setEncoding('utf-8');
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
