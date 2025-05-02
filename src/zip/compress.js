e18bff28b9b684e29cdd41eb2950ff132286cc3b
katyus666a <kruekaterinaresakevich@gmail.com>
2025-05-02 12:04:56 +0300

    feat: completed zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const inputFilePath = join('src', 'zip', 'files', 'fileToCompress.txt');
+    const outputFilePath = join('src', 'zip', 'files', 'archive.gz');
+
+    const readStream = createReadStream(inputFilePath);
+    const writeStream = createWriteStream(outputFilePath);
+    const gzip = createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
