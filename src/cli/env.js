3ffe3237d6aa1e828c837a73407f9b70ddf1c3e7
katyus666a <kruekaterinaresakevich@gmail.com>
2025-05-02 11:38:39 +0300

    feat: completed cp, moduled, hash

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
     // Write your code here 
+    const envVars = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('MITSO_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    console.log(envVars);
 };
 
+parseEnv();
 parseEnv();
--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,40 +1,39 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
-
-const random = Math.random();
-
-let unknownObject;
-
-if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
-}
-
-console.log(`Release ${release()}`);
-console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
-
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
-
-const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
-});
-
-const PORT = 3000;
-
-console.log(unknownObject);
-
-myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
-});
-
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
+import path from 'path';
+ import { release, version } from 'os';
+ import { createServer as createServerHttp } from 'http';
+ import { fileURLToPath } from 'url';
+ import fs from 'fs/promises'; 
+ import './files/c.js';
+ 
+ const random = Math.random();
+ 
+ let unknownObject;
+ if (random > 0.5) {
+     
+     unknownObject = JSON.parse(await fs.readFile(path.resolve('src/modules/files/a.json'), 'utf-8'));
+ } else {
+     
+     unknownObject = JSON.parse(await fs.readFile(path.resolve('src/modules/files/b.json'), 'utf-8'));
+ }
+ 
+ console.log(`Release ${release()}`);
+ console.log(`Version ${version()}`);
+ console.log(`Path segment separator is "${path.sep}"`);
+ 
+ console.log(`Path to current file is ${fileURLToPath(import.meta.url)}`);
+ console.log(`Path to current directory is ${path.dirname(fileURLToPath(import.meta.url))}`);
+ 
+ const myServer = createServerHttp((_, res) => {
+     res.end('Request accepted');
+ });
+ 
+ const PORT = 3000;
+ 
+ console.log(unknownObject);
+ 
+ myServer.listen(PORT, () => {
+     console.log(`Server is listening on port ${PORT}`);
+     console.log('To terminate it, use Ctrl+C combination');
+ });
+ 
+ export { unknownObject, myServer };
